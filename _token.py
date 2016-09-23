637058a2767d12fc5ea12f12a9c45df5fee8634d
luis rojas <lluirojass@aol.com>
2016-09-23 16:31:20 -0400

    string formatting approach improved

---
+++ _token.py
@@ -0,0 +1,6 @@
+#! usr/local/bin/env python3
+
+token = "MjIwNzk4OTg3Nzc3NjA1NjMy.Cqlk8Q._BVa_KoRFyB7of5xwt6HaJX4wx4"
+
+def get_token():
+    return token
--- weather.py
+++ weather.py
@@ -4,11 +4,28 @@
 import requests
 import urllib.parse as parse
 
-yql_commands = {
-    "format":"format=json",
-    'woeid':'select * from geo.places where text="{}" LIMIT 1&',
-    'forecast':'select * from weather.forecast where woeid="{}" LIMIT 1&',
-    'host':'https://query.yahooapis.com/v1/public/yql?q='
+yql_commands_toformat = {
+    'woeid':'select woeid from geo.places(1) where text="{}"&',
+    'forecast':'select * from weather.forecast(1) where woeid="{}"&',
+    'host':'https://query.yahooapis.com/v1/public/yql?q=',
+}
+
+delimeter = "&"
+i_units = "u=F"
+m_units = "u=C"
+format = "format=json"
+
+units = {
+    imperial = {
+    'distance':'',
+    'pressure':'',
+    'speed':'',
+    'temperature':''},
+    imperial = {
+    'distance':'',
+    'pressure':'',
+    'speed':'',
+    'temperature':''}
 }
 
 
@@ -20,9 +37,10 @@ def _get_woeid(place: str):
     :return: best guess WOEID based on place
     """
 
-    url = yql_commands["woeid"].format(place)
-    url = parse.quote(url,safe="&*") + yql_commands["format"]
-    query_string = yql_commands['host'] + url
+    url = yql_commands_toformat["woeid"].format(place)
+    url = parse.quote(url,safe="&*") + format
+    print(url)
+    query_string = yql_commands_toformat['host'] + url
     print(query_string)
 
     r = requests.request("GET",query_string) # throws requests.ConnectionError
@@ -30,18 +48,20 @@ def _get_woeid(place: str):
     assert results != None
 
     woeid = results['place']['woeid'] # currently throwing unpredictable KeyError due to unconsistent result type
-    print(woeid)
     return woeid
 
-
 def _get_weatherinfo(woeid):
-    host = yql_commands['host']
-    url = parse.quote(yql_commands["forecast"].format(woeid),safe="&*")+yql_commands['format']
+    """
+
+    :param woeid:
+    :return:
+    """
+    host = yql_commands_toformat['host']
+    url = parse.quote(yql_commands_toformat["forecast"].format(woeid), safe="&*") + format
     query_string = host + url
     print(query_string)
     r = requests.request("GET",query_string)
     results = r.json()['query']['results']
-
 
     # user input either too vague or very mispelled
     assert results != None
@@ -52,99 +72,50 @@ def _get_weatherinfo(woeid):
 
 def _format_data(weather_data: dict):
 
-
-    units_distance = ''
-    units_pressure = ''
-    units_temp = ''
-    units_speed = ''
-    forecast_status = ''
-    forecast_date = ''
-    forecast_day = ''
-    forecast_tlow = ''
-    forecast_thigh = ''
-    forecast  = ''
-    tom_status = ''
-    tom_date = ''
-    tom_day = ''
-    tom_tlow = ''
-    tom_thigh = ''
-    wind = ''
-    wind_chill = ''
-    wind_speed = ''
-    humidity  = ''
-    pressure = ''
-    atmosphere  = ''
-    visibility  = ''
-    astronomy = ''
-    sunrise = ''
-    sunset = ''
-
+    title = ''
+    forecast_today = "Forecast for "
+    line = '**--------------------------**\n'
+    current_conditions = 'Current Conditions: '
+    tomorrow_conditions = 'Forecast for '
 
     if 'channel' in weather_data:
         channel = weather_data['channel']
-        title = channel['title']
-        # unit information
-        #TODO bake unit demands into query string instead of wasting time extracting
-        if 'units' in channel:
-            units = channel['units']
-            units_distance = units['distance'] if 'distance' in units else ''
-            units_pressure = units['pressure'] if 'pressure' in units else ''
-            units_speed = units['speed'] if 'speed' in units else ''
-            units_temp = units['temperature'] if 'temperature' in units else ''
-        if 'wind' in channel:
-            wind = channel['wind']
-            wind_speed = wind['speed'] if 'speed' in wind else 'N/A'
-            wind_chill = wind['chill'] if 'chill' in wind else 'N/A'
-        if 'atmosphere' in channel:
-            atmosphere = channel['atmosphere']
-            humidity = atmosphere['humidity'] if 'humidity' in atmosphere else 'N/A'
-            pressure = atmosphere['pressure'] if 'pressure' in atmosphere else 'N/A'
-            #TODO find out what rising is
-            visibility = atmosphere['visibility'] if 'visibility' in atmosphere else 'N/A'
-
-        if 'astronomy' in channel:
-            astronomy = channel['astronomy']
-            sunrise = astronomy['sunrise'] if 'sunrise' in astronomy else 'N/A'
-            sunset = astronomy['sunset'] if 'sunset' in astronomy else 'N/A'
-
+        title += '{0} '.format(channel['title'])
         if 'item' in channel:
             item = channel['item']
-            # current condition information
+            # current condition & title information
             if 'condition' in item:
                 condition = item['condition']
-                current_status = condition['text'] if 'text' in condition else 'N/A'
-                current_date = condition['date'] if 'date' in condition else 'N/A'
-                current_temp = condition['temp'] if 'temp' in condition else 'N/A'
+                title += '({0})'.format(condition['date'])
+                title += ')\n'
+                current_conditions += "{0} at {1}{2}".format(condition['text'],condition['temp'],"UNITS")
             # today and tomorrow's forecast
             if 'forecast' in item:
                 forecast = item['forecast'][0]
-                forecast_status = forecast['text'] if 'text' in forecast else 'N/A'
-                forecast_date = forecast['date'] if 'date' in forecast else 'N/A'
-                forecast_day = forecast['day'] if 'day' in forecast else 'N/A'
-                forecast_tlow = forecast['low'] if 'low' in forecast else 'N/A'
-                forecast_thigh = forecast['high'] if 'high' in forecast else 'N/A'
+                forecast_today += forecast['day'] + ' ' + forecast['date'] + ' (today):' + '\n'
+                forecast_today += 'Condition: ' + forecast['text'] +'\n'
+                forecast_today += 'High: {0}{2} Low: {1}{2}\n'.format(forecast['high'],forecast['low'],"UNITS")
                 forecast = item['forecast'][1]
-            # tomorrow
-                tom_status = forecast['text'] if 'text' in forecast else 'N/A'
-                tom_date = forecast['date'] if 'date' in forecast else 'N/A'
-                tom_day = forecast['day'] if 'day' in forecast else 'N/A'
-                tom_tlow = forecast['low'] if 'low' in forecast else 'N/A'
-                tom_thigh = forecast['high'] if 'high' in forecast else 'N/A'
 
+                tomorrow_conditions += 'Forecast for {0} {1} (tomorrow):\n'.format(forecast['day'],forecast['date'])
+                tomorrow_conditions += 'Condition: {0}\n'.format(forecast['text'])
+                tomorrow_conditions += 'High: {0}{2} Low: {1}{2}'.format(forecast['high'],forecast['low'],"UNITS")
+        if 'wind' in channel:
+            wind = channel['wind']
+            forecast_today += "Wind Speed: {0}{2}  Wind Chill: {1}{3}\n".format(wind['speed'],wind['chill'],"UNITS(SPEED)","UNITS(TEMP)")
+        if 'atmosphere' in channel:
+            atmosphere = channel['atmosphere']
+            #TODO find out what rising is
+            forecast_today += 'Pressure: {0}{4} Humidity: {1}% Visibility: {2}{3}\n'.format(atmosphere['pressure'],atmosphere['humidity'],atmosphere['visibility'],"UNITS(DISTANCE)","UNITS(PRESSURE)")
+        if 'astronomy' in channel:
+            astronomy = channel['astronomy']
+            forecast_today += 'Sunrise: {0}  Sunset: {1}'.format(astronomy['sunrise'],astronomy['sunset'])
 
-    return ('{0} ({1})'.format(title,current_date)+ '\n'
-    + 'Current Conditions: {0} at {1} {2}'.format(current_status,current_temp,units_temp) + '\n'
-    + '**--------------------------**' + '\n'
-    + 'Forecast for {0} {1} (today):'.format(forecast_day,forecast_date) + '\n'
-    + 'Condition: {0}'.format(forecast_status) + '\n'
-    + 'High: {0}{2}  Low: {1}{2}'.format(forecast_thigh,forecast_tlow,units_temp) + '\n'
-    + 'Wind Speed: {0}{2}  Wind Chill: {1}{3}'.format(wind_speed,wind_chill,units_speed,units_temp) + '\n'
-    + 'Sunrise: {0}  Sunset: {1} '.format(sunrise,sunset) + '\n'
-    + 'Pressure: {0}{1} Humidity: {2}% Visibility: {3}{4}'.format(pressure,units_pressure,humidity,visibility,units_distance) + '\n'
-    + '**--------------------------**' + '\n'
-    + 'Forecast for {0} {1} (tomorrow)'.format(tom_day,tom_date) + '\n'
-    + 'Condition: {0}'.format(tom_status) + '\n'
-    + 'High: {0}{2} Low: {1}{2}'.format(tom_thigh,tom_tlow,units_temp) + '\n')
+    forecast_today +='\n'
+    current_conditions +='\n'
+    tomorrow_conditions += '\n'
+
+    return title + current_conditions + line + forecast_today  + line + tomorrow_conditions
 
     #TODO: add personalized comment based on weather code thrown. :)
 
@@ -153,7 +124,7 @@ def get_forecast(location_text: str):
     try:
         w = _get_woeid(location_text)
     except (AssertionError):
-        return "No Results for this location, maybe try more adding precision or check for misspellings"
+        return "No weather information for this location"
     except (requests.ConnectionError, KeyError, TypeError) as err:
         return "issue resolving woeid, {}".format(err.args)
 
