f2af4e39ad3d7744db9dc293c67306003c6d9ff3
luis rojas <lluisrojass@aol.com>
2017-04-29 00:53:03 -0400

    watchdog script to keep bot running

---
+++ watch.sh
@@ -0,0 +1,17 @@
+/bin/bash
+# suppress terminated messages
+(python3 ~/PycharmProjects/weather-bot/src/handler.py &  > /dev/null) 
+
+while :
+do 
+	# check if running
+	RUNPID=$(ps | egrep '((\w|\.|\d|\-)*\/)+Python ((\w|\.|\d|\-)*\/)+handler.py' | egrep -o '^\d{1,6} ')
+	if [ ${RUNPID:-1} -gt 1 ]
+	then
+		echo '(✓) - running'
+	else 
+		echo '(x) - resetting'
+		python3 ~/PycharmProjects/weather-bot/src/handler.py & 
+	fi
+	sleep $1
+done
